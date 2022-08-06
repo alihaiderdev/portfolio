a6c45e2987e85ec510bea83480b75a2d44fa451e
alihaiderdev <ah1675179@gmail.com>
2022-08-06 16:11:59 +0500

    done with reusme section 80%

--- components/Resume.jsx
+++ components/Resume.jsx
@@ -27,17 +27,59 @@ const Resume = ({
 }) => {
   return (
     <div className='resume'>
-      <span className='text-indigo-600 font-extrabold sm:text-sm md:text-lg'>
-        2014 - 2015
-      </span>
-      <h2 className='font-black font-poppins sm:text-xl md:text-2xl'>
-        {titleOrPosition}
-      </h2>
-      <h6 className='font-black font-poppins sm:text-lg md:text-xl sm:py-2 md:py-3'>
+      <div className='flex justify-between items-center'>
+        <h2 className='font-black font-poppins sm:text-2xl md:text-3xl sm:py-1 md:py-2'>
+          {titleOrPosition}
+        </h2>
+        {city && country && (
+          <span className='font-poppins'>
+            {city} / {country}
+          </span>
+        )}
+      </div>
+
+      <h6 className='font-black font-poppins sm:text-lg md:text-xl'>
         {workspaceOrCompany}
       </h6>
+      {companyDescription && (
+        <p className='text-neutral-400'>{companyDescription}</p>
+      )}
 
-      <ul className='text-neutral-400 list-decimal'>{achievementsOrTasks}</ul>
+      {/* {contactInfo && contactPerson && (
+        <ul className='text-neutral-400'>
+          <li>
+            <span className='text-indigo-600 font-poppins'>
+              Contact Person:{' '}
+            </span>
+            {contactPerson}
+          </li>
+          <li>
+            <span className='text-indigo-600 font-poppins'>Contact Info: </span>
+            {contactInfo}
+          </li>
+        </ul>
+      )} */}
+
+      <div className='flex justify-between sm:py-1 md:py-2'>
+        <h6 className='text-indigo-600 font-poppins sm:text-lg md:text-xl'>
+          Achievements/Tasks
+        </h6>
+        <span className='text-indigo-600 font-poppins sm:text-sm md:text-lg'>
+          {startDate} / {endDate ? endDate : 'Present'}
+        </span>
+      </div>
+
+      <p className='text-neutral-400'>{achievementsOrTasks}</p>
+
+      {/* <ul className='text-neutral-400 list-decimal'>
+        {achievementsOrTasks.split(',')?.map((li, index) => {
+          return (
+            <li key={index} className='marker:text-indigo-600'>
+              {li}
+            </li>
+          );
+        })}
+      </ul> */}
     </div>
   );
 };
--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,8 @@ import AboutSection from '../components/AboutSection';
 import Resume from '../components/Resume';
 import { useEffect } from 'react';
 import ResumeList from '../components/ResumeList';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 const data = [
   {
@@ -12,8 +14,8 @@ const data = [
     workspaceOrCompany: 'Codelabs Private Limited',
     startDate: '01-12-2021',
     endDate: '',
-    country: 'Pakistan',
-    city: 'Karachi',
+    // country: 'Pakistan',
+    // city: 'Karachi',
     companyDescription: '',
     achievementsOrTasks:
       'As a frontend developer here my role is to make single-page applications using react.js (projects like logofy, Ordering system) as well as creating mobile responsive, dynamic websites using HTML, CSS, JavaScript, JQuery (projects like Humwell Admin Panel a website that won Consumers Choice Award 2021 for successful services delivered in the field of healthcare and technology) and on the other side customizing and making WordPress sites like iDialogue, mywater, codelabs using Wocommerce (Elementor, Divi, WPBakery page builders) from the first day to now I have learned and explore many new things like WordPress theme based and custom development and explore many other new things like react native, next.js, GraphQL, PWA, Webrtc, stripe, socket.io, MySQL, SQL, Sequelize(ORM), styled-components, tailwindcss, etc.',
@@ -28,14 +30,16 @@ const data = [
     country: 'Pakistan',
     city: 'Karachi',
     companyDescription:
-      'Abm is a group of company and social pie its child company',
-    achievementsOrTasks: `Here at social pie my main task is to make static websites, dynamic websites, SPA, MPA using the latest technologies and trends and also make restful api's using MongoDB, expressjs and nodejs.Now a days mainly working on reactjs, nextjs apps and Strapi (Headless CMS).`,
+      'Abm is a group of company and social pie its child company.',
+    achievementsOrTasks: `Here at social pie my main task is to make static websites, dynamic websites, SPA, MPA using the latest technologies and trends and also make restful api's using MongoDB, expressjs and nodejs. Now a days mainly working on reactjs, nextjs apps and Strapi (Headless CMS).`,
     contactPerson: 'Raza Bhai',
     contactInfo: 'Front end Developer at Social Pie',
   },
 ];
 
 export default function Home() {
+  const router = useRouter();
+
   useEffect(() => {
     const links = document.querySelectorAll('.link');
     const sections = document.querySelectorAll('section');
@@ -58,7 +62,54 @@ export default function Home() {
       </section>
       <section className='pt-24' id='resume'>
         <div className='md:grid md:grid-cols-12'>
-          <div className='md:col-span-3'>sidebar</div>
+          <div className='sidebar md:col-span-3 sticky z-[1000] top-1/2'>
+            <ul>
+              <li className='ml-5 pb-1'>
+                <Link href={'/#education'} passHref>
+                  <a
+                    className={`font-poppins sm:text-sm md:text-xl ${
+                      router.asPath === '/#education' ? 'active' : ''
+                    }`}
+                  >
+                    Education
+                  </a>
+                </Link>
+              </li>
+              <li className='ml-5 pb-1'>
+                <Link href={'/#experience'} passHref>
+                  <a
+                    className={`font-poppins sm:text-sm md:text-xl ${
+                      router.asPath === '/#experience' ? 'active' : ''
+                    }`}
+                  >
+                    Experience
+                  </a>
+                </Link>
+              </li>
+              <li className='ml-5 pb-1'>
+                <Link href={'/#skill'} passHref>
+                  <a
+                    className={`font-poppins sm:text-sm md:text-xl ${
+                      router.asPath === '/#skill' ? 'active' : ''
+                    }`}
+                  >
+                    Skill
+                  </a>
+                </Link>
+              </li>
+              <li className='ml-5 pb-1'>
+                <Link href={'/#award'} passHref>
+                  <a
+                    className={`font-poppins sm:text-sm md:text-xl ${
+                      router.asPath === '/#award' ? 'active' : ''
+                    }`}
+                  >
+                    Award
+                  </a>
+                </Link>
+              </li>
+            </ul>
+          </div>
           <div className='md:col-span-9'>
             <ResumeList data={data} id='education' />
             <ResumeList data={data} id='experience' />
